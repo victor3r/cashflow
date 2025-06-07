b7672ba4e358cffbf2cee2a9d65b1d5c26161146
Victor Rabelo <victorabelo1697@gmail.com>
2025-06-07 17:59:09 -0300

    middleware for languages

---
+++ src/CashFlow.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middleware;
+
+public class CultureMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = string.IsNullOrWhiteSpace(culture) ? new CultureInfo("en") : new CultureInfo(culture);
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
