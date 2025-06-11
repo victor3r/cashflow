ab7ab3edfb6a2559873307eb62a761bd4d2a637a
Victor Rabelo <victorabelo1697@gmail.com>
2025-06-11 16:08:19 -0300

    first successful test

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Valid Expense",
+            Description = "This is a valid expense description.",
+            Amount = 100.00m,
+            Date = DateTime.Now.AddDays(-1),
+            PaymentType = PaymentType.CreditCard
+        };
+
+        var validator = new RegisterExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        Assert.True(result.IsValid);
+    }
+}
