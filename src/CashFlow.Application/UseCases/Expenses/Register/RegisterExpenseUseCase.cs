ed540f15ebe3fc76a1fa3b8caa327364fd99e82c
Victor Rabelo <victorabelo1697@gmail.com>
2025-08-22 14:50:45 -0300

    using and testing AutoMapper

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,39 +1,33 @@
-﻿using CashFlow.Communication.Requests;
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
-using CashFlow.Domain.Enums;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
-public class RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork) : IRegisterExpenseUseCase
+public class RegisterExpenseUseCase(
+    IExpensesRepository repository,
+    IUnitOfWork unitOfWork,
+    IMapper mapper) : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository = repository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
 
     public async Task<ResponseRegisteredExpenseJson> Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
 
-        var expense = new Expense
-        {
-            Title = request.Title,
-            Description = request.Description,
-            Date = request.Date,
-            Amount = request.Amount,
-            PaymentType = (PaymentType)request.PaymentType
-        };
+        var expense = _mapper.Map<Expense>(request);
 
         await _repository.Add(expense);
 
         await _unitOfWork.Commit();
 
-        return new ResponseRegisteredExpenseJson
-        {
-            Title = request.Title
-        };
+        return _mapper.Map<ResponseRegisteredExpenseJson>(expense);
     }
 
     private static void Validate(RequestRegisterExpenseJson request)
