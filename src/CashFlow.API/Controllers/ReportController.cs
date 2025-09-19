2cc7320cc9a939dc124fa649c27144866db88e5c
Victor Rabelo <victorabelo1697@gmail.com>
2025-09-19 14:38:39 -0300

    feat: creating the endpoint for the Excel file

---
+++ src/CashFlow.API/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExcel()
+    {
+        var file = new byte[1];
+
+        if (file.Length == 0)
+            return NoContent();
+
+        return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+    }
+}
