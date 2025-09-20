76d6445c4211dd53c19eceabf809f9d8242e4441
Victor Rabelo <victorabelo1697@gmail.com>
2025-09-20 15:34:01 -0300

    feat: inserting the header

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,4 +1,5 @@
-﻿using ClosedXML.Excel;
+﻿using CashFlow.Domain.Reports;
+using ClosedXML.Excel;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
@@ -11,5 +12,27 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
         workbook.Style.Font.FontName = "Arial";
 
         var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+
+        InsertHeader(worksheet);
+    }
+
+    private static void InsertHeader(IXLWorksheet worksheet)
+    {
+        var currentRow = 1;
+
+        worksheet.Cell(currentRow, 1).Value = ResourceReportGenerationMessages.TITLE;
+        worksheet.Cell(currentRow, 2).Value = ResourceReportGenerationMessages.DATE;
+        worksheet.Cell(currentRow, 3).Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+        worksheet.Cell(currentRow, 4).Value = ResourceReportGenerationMessages.AMOUNT;
+        worksheet.Cell(currentRow, 5).Value = ResourceReportGenerationMessages.DESCRIPTION;
+
+        var headerRange = worksheet.Range(currentRow, 1, currentRow, 5);
+
+        headerRange.Style.Font.Bold = true;
+        headerRange.Style.Fill.BackgroundColor = XLColor.FromHtml("#f5c2b6");
+        headerRange.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        worksheet.Cell(currentRow, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+        headerRange.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+        headerRange.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
     }
 }
