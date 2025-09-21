227225c378fcae4f56bd5d2eba21c60f70a6b828
Victor Rabelo <victorabelo1697@gmail.com>
2025-09-21 20:24:40 -0300

    feat: creating the endpoint for the PDF

--- src/CashFlow.API/Controllers/ReportController.cs
+++ src/CashFlow.API/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -21,5 +22,20 @@ public class ReportController : ControllerBase
             return NoContent();
 
         return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+    }
+
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf(
+       [FromServices] IGenerateExpensesReportPdfUseCase useCase,
+       [FromQuery] DateOnly month)
+    {
+        var file = await useCase.Execute(month);
+
+        if (file.Length == 0)
+            return NoContent();
+
+        return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Repositories.Expenses;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+public class GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository) : IGenerateExpensesReportPdfUseCase
+{
+    private const string CURRENCY_SYMBOL = "R$";
+    private readonly IExpensesReadOnlyRepository _repository = repository;
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _repository.FilterByMonth(month);
+
+        if (expenses.Count == 0)
+        {
+            return [];
+        }
+
+        return [];
+    }
+}
